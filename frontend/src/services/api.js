b1f451ed051f8665a3ece8b9edd4aca2ef4d86a3
arunveguru <arunveguru2005@gmail.com>
2026-01-01 23:13:28 +0530

    Changed backend url in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/predict";
+const API_URL = "https://student-performance-api-14pb.onrender.com/predict";
 
 export async function predictScore(formData) {
   const response = await fetch(API_URL, {
